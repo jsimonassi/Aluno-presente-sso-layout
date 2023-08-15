7bb4a31d233d8d962a60559c6143956514a20e05
jsimonassi <jsimonassi@id.uff.br>
2023-08-15 00:00:51 -0300

    Finish new account

--- script.js
+++ script.js
@@ -12,20 +12,20 @@ const doLogin = () => {
 
     if(!validateEmail(email)) {
         document.getElementById('input_email').style.border = '1px solid rgb(171, 11, 11)';
-        document.getElementById('emailError').style.display = 'block';
+        document.getElementById('email_error').style.display = 'block';
         allRight = false;
     } else {
         document.getElementById('input_email').style.border = 'none';
-        document.getElementById('emailError').style.display = 'none';
+        document.getElementById('email_error').style.display = 'none';
     }
 
     if(password == '') {
         document.getElementById('input_password').style.border = '1px solid rgb(171, 11, 11)';
-        document.getElementById('passwordError').style.display = 'block';
+        document.getElementById('email_error').style.display = 'block';
         allRight = false;
     } else {
         document.getElementById('input_password').style.border = 'none';
-        document.getElementById('passwordError').style.display = 'none';
+        document.getElementById('email_error').style.display = 'none';
     }
 
     if(!allRight) {
@@ -41,12 +41,66 @@ const recoverPassword = () => {
 
     if(!validateEmail(email)) {
         document.getElementById('input_email').style.border = '1px solid rgb(171, 11, 11)';
-        document.getElementById('emailError').style.display = 'block';
+        document.getElementById('email_error').style.display = 'block';
         return;
     } else {
         document.getElementById('input_email').style.border = 'none';
-        document.getElementById('emailError').style.display = 'none';
+        document.getElementById('email_error').style.display = 'none';
     }
 
     alert(`Email: ${email}`);
+}
+
+
+const signUp = () => {
+    let name = document.getElementById('input_name').value;
+    let email = document.getElementById('input_email').value;
+    let password = document.getElementById('input_password').value;
+    let passwordConfirm = document.getElementById('input_password_confirmation').value;
+
+    let allRight = true;
+
+    console.log("name: ", name);
+    
+    if(name == '') {
+        document.getElementById('input_name').style.border = '1px solid rgb(171, 11, 11)';
+        document.getElementById('name_error').style.display = 'block';
+        allRight = false;
+    } else {
+        document.getElementById('input_name').style.border = 'none';
+        document.getElementById('name_error').style.display = 'none';
+    }
+
+    if(!validateEmail(email)) {
+        document.getElementById('input_email').style.border = '1px solid rgb(171, 11, 11)';
+        document.getElementById('email_error').style.display = 'block';
+        allRight = false;
+    } else {
+        document.getElementById('input_email').style.border = 'none';
+        document.getElementById('email_error').style.display = 'none';
+    }
+
+    if(password == '') {
+        document.getElementById('input_password').style.border = '1px solid rgb(171, 11, 11)';
+        document.getElementById('password_error').style.display = 'block';
+        allRight = false;
+    } else {
+        document.getElementById('input_password').style.border = 'none';
+        document.getElementById('password_error').style.display = 'none';
+    }
+
+    if(passwordConfirm != password) {
+        document.getElementById('input_password_confirmation').style.border = '1px solid rgb(171, 11, 11)';
+        document.getElementById('password_confirmation_error').style.display = 'block';
+        allRight = false;
+    } else {
+        document.getElementById('input_password_confirmation').style.border = 'none';
+        document.getElementById('password_confirmation_error').style.display = 'none';
+    }
+
+    if(!allRight) {
+        return;
+    }
+
+    alert(`Email: ${email} Password: ${password}`);
 }
